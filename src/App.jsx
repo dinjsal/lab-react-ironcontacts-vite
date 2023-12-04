0d63579bb1170e9a857f1e47f06d282f1c6f816a
Dinah Salazar <dinahsalazar@gmail.com>
2023-12-04 12:49:20 +0900

    Complete iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import "./App.css";
+import { useState } from "react";
+import contactsList from "./contacts.json";
 
 function App() {
+  const [contacts, setContacts] = useState(contactsList);
   return (
     <div className="App">
-      <h1>LAB | React IronContacts</h1>
+      <h1>IronContacts</h1>
+          <div key={contacts.id} className="contact-list">
+          <table>
+                <thead>
+                <tr>
+                  <th>Picture</th>
+                  <th>Name</th>
+                  <th>Popularity</th>
+                </tr>
+                </thead>
+            {contacts
+            .slice(0,5)
+            .map((oneContact) => {
+              return (
+                <tbody>
+                  <tr>
+                    <td><img src={oneContact.pictureUrl} alt={oneContact.name} style={{height: "150px"}} /></td>
+                    <td>{oneContact.name}</td>
+                    <td>{oneContact.popularity}</td>
+                  </tr>
+                </tbody>
+              )
+            })}
+            </table>
+          </div>
     </div>
   );
 }
