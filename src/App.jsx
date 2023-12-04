6b435f3b440d3f6172fd9907ec65f20f330d9360
Dinah Salazar <dinahsalazar@gmail.com>
2023-12-04 16:03:39 +0900

    Complete iteration 2, ternary + emojis

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ function App() {
                   <th>Picture</th>
                   <th>Name</th>
                   <th>Popularity</th>
+                  <th>Won an Oscar</th>
+                  <th>Won an Emmy</th>
                 </tr>
                 </thead>
             {contacts
@@ -25,6 +27,8 @@ function App() {
                     <td><img src={oneContact.pictureUrl} alt={oneContact.name} style={{height: "150px"}} /></td>
                     <td>{oneContact.name}</td>
                     <td>{oneContact.popularity}</td>
+                    <td>{oneContact.wonOscar ? "🏆" : ""}</td>
+                    <td>{oneContact.wonEmmy ? "🌟" : ""}</td>
                   </tr>
                 </tbody>
               )
